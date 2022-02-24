bfbe88530b28bbe9f11f890b3c3fe56a1ff06815
손정훈 <sjh060623@gmail.com>
2022-02-24 14:19:53 +0900

    coin_tracker 1.12

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -219,7 +219,7 @@ function Coin() {
               <Price />
             </Route>
             <Route path={`/:coinId/chart`}>
-              <Chart />
+              <Chart coinId={coinId} />
             </Route>
           </Switch>
         </>
