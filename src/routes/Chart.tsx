b5b3b2ac0bb1304d9ec28102deef699dee1ee22d
손정훈 <sjh060623@gmail.com>
2022-02-24 14:45:08 +0900

    coin_tracker 1.13

--- src/routes/Chart.tsx
+++ src/routes/Chart.tsx
@@ -1,16 +1,74 @@
 import { useQuery } from "react-query";
 import { fetchCoinHistory } from "../api";
-
+import ApexChart from "react-apexcharts";
+import { chdir } from "process";
+import { theme } from "../theme";
+interface IHistorical {
+  time_open: string;
+  time_close: string;
+  open: number;
+  high: number;
+  low: number;
+  close: number;
+  volume: number;
+  market_cap: number;
+}
 interface ChartProps {
   coinId: string;
 }
 
 function Chart({ coinId }: ChartProps) {
-  const { isLoading, data } = useQuery(
-    ["ohlcv", coinId],
-    () => fetchCoinHistory
+  const { isLoading, data } = useQuery<IHistorical[]>(["ohlcv", coinId], () =>
+    fetchCoinHistory(coinId)
   );
-  return <h1>Chart</h1>;
+  return (
+    <div>
+      {isLoading ? (
+        "Loading"
+      ) : (
+        <ApexChart
+          type="line"
+          series={[
+            {
+              name: "price",
+              data: data?.map((price) => price.close),
+            },
+          ]}
+          options={{
+            theme: {
+              mode: "dark",
+            },
+
+            chart: {
+              width: 500,
+              height: 500,
+              background: "transparent",
+              toolbar: {
+                show: false,
+              },
+            },
+            grid: {
+              show: false,
+            },
+            stroke: {
+              curve: "smooth",
+              width: 4,
+            },
+            yaxis: {
+              show: false,
+            },
+            xaxis: {
+              axisTicks: { show: false },
+              axisBorder: { show: false },
+              labels: {
+                show: false,
+              },
+            },
+          }}
+        />
+      )}
+    </div>
+  );
 }
 
 export default Chart;
