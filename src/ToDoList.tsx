64f945ace4420c61691218396c428f824f217781
손정훈 <sjh060623@gmail.com>
2022-02-27 23:07:45 +0900

    stage_management 1.9

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -36,6 +36,7 @@ interface IForm {
   username: string;
   password: string;
   password1: string;
+  extraError?: string;
 }
 
 function ToDoList() {
@@ -43,14 +44,23 @@ function ToDoList() {
     register,
     handleSubmit,
     formState: { errors },
+    setError,
   } = useForm<IForm>({
     defaultValues: {
       email: "@naver.com",
     },
   });
-  const onValid = (data: any) => {
-    console.log(data);
+  const onValid = (data: IForm) => {
+    if (data.password !== data.password1) {
+      setError(
+        "password1",
+        { message: "Password are not the same" },
+        { shouldFocus: true }
+      );
+    }
+    // setError("extraError", { message: "Server offline." });
   };
+  console.log(errors);
   return (
     <div>
       <form
@@ -69,7 +79,15 @@ function ToDoList() {
         />
         <span>{errors?.email?.message}</span>
         <input
-          {...register("firstName", { required: "write here" })}
+          {...register("firstName", {
+            required: "write here",
+            validate: {
+              noNico: (value) =>
+                value.includes("nico") ? "no nicos allowed" : true,
+              noNick: (value) =>
+                value.includes("nick") ? "no nick allowed" : true,
+            },
+          })}
           placeholder="First Name"
         />
         <span>{errors?.firstName?.message}</span>
@@ -81,7 +99,7 @@ function ToDoList() {
         <input
           {...register("username", {
             required: "write here",
-            minLength: { value: 10, message: "more 10" },
+            minLength: { value: 5, message: "more 5" },
           })}
           placeholder="Username"
         />
@@ -89,7 +107,7 @@ function ToDoList() {
         <input
           {...register("password", {
             required: "write here",
-            minLength: { value: 5, message: "more 5" },
+            minLength: { value: 10, message: "more 10" },
           })}
           placeholder="Password"
         />
@@ -106,6 +124,7 @@ function ToDoList() {
         />
         <span>{errors?.password1?.message}</span>
         <button>Add</button>
+        <span>{errors?.extraError?.message}</span>
       </form>
     </div>
   );
